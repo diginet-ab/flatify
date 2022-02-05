4ac32105c47cdee9e466ae035801597a4ac1d62b
Anders Westberg <anders.westberg@diginet.se>
2022-02-05 23:00:26 +0100

    Updates

--- cli.ts
+++ cli.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import fsp from "fs/promises"
-import path from "path"
 import { program } from 'commander'
 import 'ts-replace-all'
+import readJson from 'read-package-json'
 
 type Options = {
     base: string
@@ -31,13 +31,13 @@ export const copyWebBuildFilesToFlatFolder = async (sourcePath: string, destPath
     const files = await getAllFiles(sourcePath, '')
     const fileMap: { publicName: string, localName: string }[] = []
     for (const [index, publicName] of files.entries()) {
-        const localName = `${ options.base }${index.toString()}${ options.extension }`
-        const destName = `${destPath}\\${ localName }`
+        const localName = `${options.base}${index.toString()}${options.extension}`
+        const destName = `${destPath}\\${localName}`
         if (options.debug || options.test) {
             if (!options.test)
-                console.log(`Copying ${ publicName } to ${ destName }`)
+                console.log(`Copying ${publicName} to ${destName}`)
             else
-                console.log(`Would copy ${ publicName } to ${ destName }`)
+                console.log(`Would copy ${publicName} to ${destName}`)
         }
         if (!options.test)
             await fsp.copyFile(sourcePath + '/' + publicName, destName)
@@ -45,7 +45,7 @@ export const copyWebBuildFilesToFlatFolder = async (sourcePath: string, destPath
     }
     const modifiedTime = new Date()
     const modifiedTimeString = modifiedTime.toDateString() + ' ' + modifiedTime.toTimeString()
-    await fsp.writeFile(destPath + `\\${ options.json }`, JSON.stringify({ fileMap, date: modifiedTimeString }, undefined, 2))
+    await fsp.writeFile(destPath + `\\${options.json}`, JSON.stringify({ fileMap, date: modifiedTimeString }, undefined, 2))
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -56,23 +56,45 @@ const sprintf = (strings: TemplateStringsArray, ...indices: number[]) => {
         )
 }
 
-const defaultSource = './'
-const defaultTarget = './output'
-
-program
-    .name('flatify')
-    .description('CLI to flatten a source folder with hierarchical folder structure to a flat list of numbered files in a target folder. A JSON file is also output containing an array of the original names.')
-    .version('0.0.0')
-    .option('-b, --base <name>', 'Flat file base name', 'file')
-    .option('-e, --extension <ext>', 'Flat file extension', '.bin')
-    .option('-j, --json <name>', 'JSON file name', 'files.json')
-    .option('-d, --debug', 'Debug info')
-    .option('-t, --test', 'Display files but do not copy')
-    .argument('[source]', 'source folder', defaultSource)
-    .argument('[target]', 'target folder', defaultTarget)
-    .action(async (source, target, options: Options, command) => {
-        await copyWebBuildFilesToFlatFolder(source, target, options)
+const getVersion = (): Promise<string> => {
+    const p = new Promise<string>((resolve, reject) => {
+        readJson('./package.json', undefined, false, (er: any, data: any) => {
+            if (!er)
+                resolve(data.version as string)
+            else
+                reject(er)
+        })
     })
+    return p
+}
 
-program.parse()
+const main = async () => {
+    const defaultSource = './'
+    const defaultTarget = './output'
 
+    let version = "?"
+    try {
+        version = await getVersion()
+    } catch {
+    }
+    program
+        .name('flatify')
+        .description('CLI to flatten a source folder with hierarchical folder structure to a flat list of numbered files in a target folder. A JSON file is also output containing an array of the original names.')
+        .version('0.0.0')
+        .option('-b, --base <name>', 'Flat file base name', 'file')
+        .option('-e, --extension <ext>', 'Flat file extension', '.bin')
+        .option('-j, --json <name>', 'JSON file name', 'files.json')
+        .option('-d, --debug', 'Debug info')
+        .option('-t, --test', 'Display files but do not copy')
+        .argument('[source]', 'source folder', defaultSource)
+        .argument('[target]', 'target folder', defaultTarget)
+        .action(async (source, target, options: Options, command) => {
+            await copyWebBuildFilesToFlatFolder(source, target, options)
+        })
+        .version(version)
+
+    program.parse()
+
+}
+
+main()
